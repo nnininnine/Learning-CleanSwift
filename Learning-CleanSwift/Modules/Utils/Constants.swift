bf04fc6dba3d62e53ce114c59a7cf32c7fab1621
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-17 11:35:30 +0700

    feat: add constants

---
+++ Learning-CleanSwift/Modules/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Learning-CleanSwift
+//
+//  Created by 7Peaks on 17/6/2565 BE.
+//
+
+import Foundation
+
+enum Constants {
+    static let nameDefaultsKey: String = "nameDefaultsKey"
+}
